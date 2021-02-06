968835f06f1112d116d1504ba161f8cddfd21786
wingedrasengan927 <ms.neerajkrishna@gmail.com>
2021-02-06 10:49:49 -0800

    Add default PORT

--- database/base.py
+++ database/base.py
@@ -6,8 +6,8 @@ from sqlalchemy.orm import sessionmaker
 
 USERNAME = os.environ.get("POSTGRES_USER", "mypguser")
 PASSWORD = os.environ.get("POSTGRES_PASSWORD", "mypgpassword")
-PORT = os.environ.get("PORT")
-DBNAME = os.environ.get("POSTGRES_DB", "sqlalchemy")
+PORT = os.environ.get("PORT", "5432")
+DBNAME = os.environ.get("POSTGRES_DB", "customer_reviews")
 CONTAINER_NAME = os.environ.get("CONTAINER_NAME", "db")
 
 engine = create_engine('postgresql://{}:{}@{}:{}/{}'.format(USERNAME, PASSWORD, CONTAINER_NAME, PORT, DBNAME))
